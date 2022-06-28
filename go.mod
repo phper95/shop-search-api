21630cdc0fe1394aa5f92fbc3a009047aa4d012b
phper95 <1366295050@qq.com>
2022-06-28 22:11:42 +0800

    gitignore

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	gitee.com/phper95/pkg/cache v0.0.0-20220320105256-ad9d98ffcf0b
 	gitee.com/phper95/pkg/db v0.0.0-20220320094248-fb3254efc09f
+	gitee.com/phper95/pkg/docker v0.0.0-20220329234458-2cfbe7f00626
 	gitee.com/phper95/pkg/errors v0.0.0-20220305150109-4dedf954f7fb
 	gitee.com/phper95/pkg/httpclient v0.0.0-20220320125830-ab0e153a1903
 	gitee.com/phper95/pkg/logger v0.0.0-20220320082302-c3a996a8a9df
@@ -14,7 +15,6 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-redis/redis/v7 v7.4.1
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.10.1
 	github.com/valyala/fasthttp v1.34.0
 	go.uber.org/zap v1.21.0
--- script/test/test_setup/setup_test_middleware_dependency.go
+++ script/test/test_setup/setup_test_middleware_dependency.go
@@ -2,37 +2,74 @@ package main
 
 import (
 	"fmt"
+	"gitee.com/phper95/pkg/docker"
 	"github.com/valyala/fasthttp"
 	"net/http"
-	"shop-search-api/internal/pkg/docker"
 	"sync"
 	"time"
 )
 
 const (
 	StartTimeoutSecond = 180
-	ESUser             = "elastic"
 	User               = "test"
-	//注意，ES密码需要大于6位
-	Pass = "unit-test"
+	Pass               = "unit-test"
 )
-
-var ESDocker *docker.Docker
-var mysqlDocker *docker.Docker
 
 func main() {
 	InitMiddleware()
 }
 
-func InitMiddleware() {
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	//go startMysql(&wg)
-	go startES(&wg)
-	wg.Wait()
+func checkESSever() bool {
+	url := "http://localhost:9200"
+	for ticker := 1; ticker < StartTimeoutSecond; ticker++ {
+		httpCode, _, _ := fasthttp.Get(nil, url)
+		if httpCode == http.StatusOK {
+			return true
+		}
+		time.Sleep(time.Second)
+		fmt.Println("check ES", ticker)
+	}
+	return false
 }
 
-func startMysql(wg *sync.WaitGroup) {
+func startES(wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+	containerOption := docker.ContainerOption{
+		Name:      "elastic-unittest",
+		ImageName: "phper95/es8.1.0",
+		Options: map[string]string{
+			"xpack.security.enabled": "false",
+			"discovery.seed_hosts":   "127.0.0.1:9300",
+			"discovery.type":         "single-node",
+		},
+		MountPath:  "/usr/share/elasticsearch/data",
+		PortExpose: "9200",
+	}
+	ESDocker := &docker.Docker{}
+	if !ESDocker.IsInstalled() {
+		panic("docker has`t install")
+	}
+	err := ESDocker.RemoveIfExists(containerOption)
+	if err != nil {
+		panic(err)
+	}
+	res, err := ESDocker.Start(containerOption)
+	if err != nil {
+		fmt.Println(res)
+		panic(err)
+	}
+	fmt.Println("docker", containerOption.ImageName, "has started")
+	if checkESSever() {
+		fmt.Println("es sever has started")
+	} else {
+		fmt.Println("es sever started timeout")
+		ESDocker.RemoveIfExists(containerOption)
+	}
+}
+
+func StartMysql(wg *sync.WaitGroup) {
 	defer func() {
 		wg.Done()
 	}()
@@ -42,17 +79,17 @@ func startMysql(wg *sync.WaitGroup) {
 		"MYSQL_PASSWORD":      Pass,
 		"MYSQL_DATABASE":      "shop",
 	}
-	containerOption := docker.ContainerOption{
-		Name:              "mysql-unittest",
-		Options:           mysqlOptions,
-		MountVolumePath:   "/var/lib/mysql",
-		PortExpose:        "3306",
-		ContainerFileName: "mysql:5.7",
-	}
 
-	mysqlDocker = &docker.Docker{}
+	containerOption := docker.ContainerOption{
+		Name:       "mysql-unittest",
+		ImageName:  "mysql:5.7",
+		Options:    mysqlOptions,
+		MountPath:  "/var/lib/mysql",
+		PortExpose: "3306",
+	}
+	mysqlDocker := docker.Docker{}
 	if !mysqlDocker.IsInstalled() {
-		panic("docker has`t installed")
+		panic("docker has`t install")
 	}
 	err := mysqlDocker.RemoveIfExists(containerOption)
 	if err != nil {
@@ -63,65 +100,15 @@ func startMysql(wg *sync.WaitGroup) {
 		fmt.Println(res)
 		panic(err)
 	}
-	err = mysqlDocker.WaitForStartOrKill(StartTimeoutSecond)
-	if err != nil {
-		panic("mysql server start timeout")
-	}
-	fmt.Println("docker", containerOption.ContainerFileName, "has started")
+	mysqlDocker.WaitForStartOrKill(StartTimeoutSecond)
+	fmt.Println("mysql has started")
 }
 
-func startES(wg *sync.WaitGroup) {
-	defer func() {
-		wg.Done()
-	}()
-	containerOption := docker.ContainerOption{
-		Name: "elastic-unittest",
-		Options: map[string]string{
-			"xpack.security.enabled": "false",
-			"discovery.seed_hosts":   "127.0.0.1:9300",
-			"discovery.type":         "single-node"},
-		MountVolumePath:   "/usr/share/elasticsearch/data",
-		PortExpose:        "9200",
-		ContainerFileName: "phper95/es8.1.0",
-		//ContainerFileName: "docker.elastic.co/elasticsearch/elasticsearch:8.1.0",
-	}
+func InitMiddleware() {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go startES(&wg)
+	go StartMysql(&wg)
+	wg.Wait()
 
-	//cmd := fmt.Sprintf("docker exec -it %s /usr/share/elasticsearch/bin/elasticsearch-users useradd %s -p %s -r superuser", containerOption.Name, User, Pass)
-	//network := "elastic"
-	ESDocker = &docker.Docker{}
-	if !ESDocker.IsInstalled() {
-		panic("docker has`t installed")
-	}
-	err := ESDocker.RemoveIfExists(containerOption)
-	if err != nil {
-		panic(err)
-	}
-	//ESDocker.CreateNetwork(network)
-	res, err := ESDocker.Start(containerOption)
-	if err != nil {
-		fmt.Println(res)
-		panic(err)
-	}
-	fmt.Println("docker", containerOption.ContainerFileName, "has started")
-	//检测服务是否就绪
-	if checkESServer() {
-		fmt.Println("es server started")
-	} else {
-		fmt.Println("es server start timeout, going to remove es sever")
-		ESDocker.RemoveIfExists(containerOption)
-	}
-}
-
-func checkESServer() bool {
-	url := "http://localhost:9200"
-	fmt.Println(url)
-	for tick := 1; tick <= StartTimeoutSecond; tick++ {
-		httpCode, _, _ := fasthttp.Get(nil, url)
-		if httpCode == http.StatusOK {
-			return true
-		}
-		time.Sleep(time.Second)
-		fmt.Println("check cost ", tick)
-	}
-	return false
 }
